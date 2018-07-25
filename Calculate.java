2e9755ebe6c4de34f0f9fa138085a8d443558b76
Melomanshik <41650122+Melomanshik@users.noreply.github.com>
2018-07-25 09:19:00 +0300

    Add files via upload

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,10 @@
 public class Calculate {
 	static void main(String[] arg) {
+		
 		System.out.println("Calculate");
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+		int sum = first + second;
+		System.out.println("sum - " + sum);
 	}
 }
